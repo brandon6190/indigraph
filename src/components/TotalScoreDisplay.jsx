f4811b6c8a2e78a7829a9399b87e3c9ce725ae28
Brandon Marrero <brandonm6190@gmail.com>
2025-04-29 22:48:34 -0400

    cleaned up line for total score

--- src/components/TotalScoreDisplay.jsx
+++ src/components/TotalScoreDisplay.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 export default function TotalScoreDisplay({ scores, onSubmit }) {
   const scoreValues = Object.values(scores);
-  const total = scoreValues.length === 0 
-  ? 0 
-  : (scoreValues.reduce((a, b) => a + b, 0) / scoreValues.length).toFixed(2);
+  const total = scoreValues.length ? (scoreValues.reduce((acc, val) => acc + val, 0) / scoreValues.length).toFixed(2) : 0;
   
   return (
     <div className="total-score-display">
