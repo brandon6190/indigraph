549b77abaf40c905c1c45e25961300b9d2b6a797
Brandon Marrero <brandonm6190@gmail.com>
2025-05-05 16:13:35 -0400

    Created a componenet for the Chart title and added the styiling to align it at the top and center. The title changes based on the current month and coin selected

---
+++ src/components/ScoreChartTitle.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function scoreChartTitle({coin, currentMonth}) {
+    const months = {
+        '01': 'JAN',
+        '02': 'FEB',
+        '03': 'MAR',
+        '04': 'APR',
+        '05': 'MAY',
+        '06': 'JUNE',
+        '07': 'JULY',
+        '08': 'AUG',
+        '09': 'SEP',
+        '10': 'OCT',
+        '11': 'NOV',
+        '12': 'DEC',
+    }
+
+    const [month, year] = currentMonth.split('-');
+
+    return <h2 className="score-chart-title">{coin} - {months[month]} {year}</h2>;
+}
