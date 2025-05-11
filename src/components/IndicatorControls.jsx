bc82632f3f80962d02b7850f1885f0473d701e1d
Brandon Marrero <brandonm6190@gmail.com>
2025-05-11 14:59:56 -0400

    Users are now able to add indicators by writing their name in an input element and hitting the add button, and users can also remove indicators by hitting the x button that each indicator has. Removing and adding indicators effect the total score.

--- src/components/IndicatorControls.jsx
+++ src/components/IndicatorControls.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
 
-export default function IndicatorControls({ indicators, scores, onScoreChange }) {
+export default function IndicatorControls({ indicators, scores, onScoreChange, onAddIndicator, onRemoveIndicator }) {
+  const [newIndicator, setNewIndicator] = React.useState('');
+
+  const handleAdd = () => {
+    onAddIndicator(newIndicator);
+    setNewIndicator('');
+  };
+  
   return (
     <div className="indicator-controls">
-      {indicators.map(ind => (
-        <div key={ind} className="indicator-list">
-          <span className="indicator-name">{ind}</span>
-          <div className="indicator-btns">
-            <button 
-              className={`indicator-up-btn ${scores[ind] === 1 ? 'selected' : ''}`}
-              onClick={() => onScoreChange(ind, 1)}>
-              Up
-            </button>
-            <button 
-              className={`indicator-down-btn ${scores[ind] === -1 ? 'selected' : ''}`} 
-              onClick={() => onScoreChange(ind, -1)}>
-              Down
-            </button>
+      <div className="indicator-add">
+        <input 
+          type="text" 
+          value={newIndicator} 
+          onChange={e => setNewIndicator(e.target.value)} 
+          placeholder="Add Indicator" />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+
+      <div className="indicator-list">
+        {indicators.map(ind => (
+          <div key={ind} className="indicator">
+            <span className="indicator-name">{ind}</span>
+            <div className="indicator-btns">
+              <button 
+                className={`indicator-up-btn ${scores[ind] === 1 ? 'selected' : ''}`}
+                onClick={() => onScoreChange(ind, 1)}>
+                Up
+              </button>
+              <button 
+                className={`indicator-down-btn ${scores[ind] === -1 ? 'selected' : ''}`} 
+                onClick={() => onScoreChange(ind, -1)}>
+                Down
+              </button>
+              <button 
+                className="indicator-remove-btn" 
+                onClick={() => onRemoveIndicator(ind)}>
+                  ✕
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
