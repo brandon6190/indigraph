c7f25ba4edd556afc18a995732f1ef7310515473
Brandon Marrero <brandonm6190@gmail.com>
2025-05-14 20:50:48 -0400

    Enabled tooltip for chart

--- src/components/ScoreChart.jsx
+++ src/components/ScoreChart.jsx
@@ -74,7 +74,7 @@ export default function ScoreChart({ data }) {
     },
     plugins: {
         legend: { display: false },
-        tooltip: { enabled: false }
+        tooltip: { enabled: true }
     }
   };
 
